5ef32cee8b7230184591c34bdc47609dca4e86a0
t00lss <nlegend411@gmail.com>
2026-07-19 08:52:06 -0400

    Create commandHandler.js

---
+++ handlers/commandHandler.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+module.exports = (client) => {
+
+    client.commands = new Map();
+
+    const commandFiles = fs.readdirSync("./commands")
+        .filter(file => file.endsWith(".js"));
+
+
+    for (const file of commandFiles) {
+
+        const command = require(`../commands/${file}`);
+
+        client.commands.set(
+            command.name,
+            command
+        );
+
+        console.log(
+            `Loaded command: ${command.name}`
+        );
+    }
+
+};
